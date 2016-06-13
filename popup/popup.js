5b8bcdcb2a55cb4e71e5ab8c34fb7c6de8adb76b
Ahmed Khattak <khattak.ahmed@yahoo.com>
2016-06-13 17:05:55 +0500

    moved popup related files to their own folder

---
+++ popup/popup.js
@@ -0,0 +1,13 @@
+$("#profile").click(function() {
+    window.open('chrome-extension://pncbccaloljmhkjangggfdmjefbdlfei/StudentProfile/Home.html', '_blank');
+});
+
+
+$("#github").click(function() {
+    window.open('https://github.com/AhmedKhattak/Student-Profile-Enhancer', '_blank');
+});
+
+
+$("#refresh").click(function() {
+    alert("Handler for .click() called.");
+});
